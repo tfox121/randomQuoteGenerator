f595bcc85e528daebcad339e514c6af537395736
Tom Fox <tfox2@hotmail.co.uk>
2019-07-23 01:28:06 +0100

    Separate out nested functions. Fix animation issue. Change XMLHTTP request to JQuery AJAX.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
+let json
+
 $(document).ready(function() {
-    function reqListener() {
-      json = JSON.parse(this.responseText);
-      function quoteGenerator(num) {
-        return json["quotes"][num];
-      }
-  
-      document.getElementById("newQuote").onclick = function() {
-        $(".fader").removeClass("fade-in");
-        $(".fader").addClass("fade-out");
-        setTimeout(function() {
-          function getRandomInt() {
-            return Math.floor(Math.random() * (json["quotes"].length + 1));
-          }
-          let randomQuote = quoteGenerator(getRandomInt());
-          document.getElementsByClassName("quote")[0].innerHTML =
-            "&nbsp;" + randomQuote["quote"];
-          document.getElementsByClassName("quoter")[0].innerHTML =
-            "- " + randomQuote["author"];
-          $(".fader").removeClass("fade-out");
-          $(".fader").addClass("fade-in");
-        }, 1500);
-      };
-    }
-  
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open(
-      "GET",
-      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-    );
-    oReq.send();
-  
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  });
-  
+  $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json", success: function(result){
+    json = JSON.parse(result)
+    quoteSetter();
+  }});
+});
+
+function quoteGenerator(num, json) {
+  return json["quotes"][num];
+}
+
+function getRandomInt(json) {
+  return Math.floor(Math.random() * (json["quotes"].length + 1));
+}
+
+$('#newQuote').on('click', function(){
+  $('#quotewrap').fadeOut('slow', function() {
+    quoteSetter()
+  }).fadeIn('slow')
+  $('#quoter').fadeOut('slow').fadeIn('slow')
+  //$('.color-changer').css('background-color', getRandomColor)
+});
+
+function quoteSetter() {
+  let randomQuote = quoteGenerator(getRandomInt(json), json);
+  $("#quote").html("&nbsp;" + randomQuote["quote"]);
+  $("#quoter").html("- " + randomQuote["author"]);
+}
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
